6a63aea85c82e11f5a15436dbeffd015d510266d
victortwu <victor607@gmail.com>
2021-04-29 08:02:25 -0700

    added some more styling

--- frontend_portfolio/src/App.js
+++ frontend_portfolio/src/App.js
@@ -11,16 +11,25 @@ render() {
     <div className="App">
     <nav>
       <div className='navDiv'>
-          <h4>GitHub</h4>
-          <h4>LinkedIn</h4>
-          <h4>Resume</h4>
+          <h4 id='githubLink'>GitHub</h4>
+          <h4 id='linkedInLink'>LinkedIn</h4>
+          <h4 id='resumeLink'>Resume</h4>
       </div>
     </nav>
 
-        <h1>VICTOR TWU</h1>
-        <h3>software developer</h3>
-        <h5>brand statement</h5>
+        <h1>VICTOR <span id='twu'>TWU</span></h1>
+        <h3><span id='software'>SOFTWARE</span> DEVELOPER</h3>
+        <div className='projectsDiv'>
+
+
+        </div>
+
+        <h4 id='about'>ABOUT</h4>
+        <div className='brandStatementDiv'>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. </p>
+        </div>
+        <footer>
+        </footer>
     </div>
   )
 }
