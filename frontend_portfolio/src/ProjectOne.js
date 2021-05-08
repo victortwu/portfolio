8d0a21ada9b53d41cd1a820743f627f444b56ba7
victortwu <victor607@gmail.com>
2021-05-08 09:41:06 -0700

    added 3d flip card

--- frontend_portfolio/src/ProjectOne.js
+++ frontend_portfolio/src/ProjectOne.js
@@ -3,7 +3,19 @@
 const ProjectOne = () => {
     return(
       <div className='projectCard'>
-        project 1
+      <div class="card__content">
+
+             <div className="card__front">
+               <h3 className="card__title">COUNTING GAME</h3>
+               <p className="card__subtitle">T - REX</p>
+             </div>
+
+             <div className="card__back">
+               <p className="card__body">This would be some longer text that gives a description of the things from the other side I guess</p>
+             </div>
+
+            </div>
+
       </div>
 
 
