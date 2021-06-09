db6d3f47e284400ad39dfbb8c5bcabf0f4589c12
victortwu <victor607@gmail.com>
2021-06-09 09:12:58 -0700

    reorganized tags for structure, preparing for css grid

--- src/App.js
+++ src/App.js
@@ -35,52 +35,64 @@ render() {
       return (
       <main className={toggleBackGround}>
         <div className="App">
-        <nav>
-              <div className='navDiv'>
-                  {
-                    !this.state.lightMode
-                    ? <h4 onClick={()=> this.toggleDarkMode()}>Bright Mode</h4>
-                    : <h4 onClick={()=> this.toggleLightMode()}>Dark Mode</h4>
-                  }
-                  <h4 id='githubLink'><a href='https://github.com/victortwu' target='none'>GitHub</a></h4>
-                  <h4 id='linkedInLink'><a href='https://www.linkedin.com/in/victor-twu-6292631b1/' target='none'>LinkedIn</a></h4>
-                  <h4 id='resumeLink'><a href='https://docs.google.com/document/d/17n-inD_l5K_zb6C8aknWffRcIbdIMsC7_K1dTo6vpYM/edit?usp=sharing' target='none'>Resume</a></h4>
-              </div>
 
+                <nav>
+                      <div className='navDiv'>
+                          {
+                            !this.state.lightMode
+                            ? <h4 onClick={()=> this.toggleDarkMode()}>Bright Mode</h4>
+                            : <h4 onClick={()=> this.toggleLightMode()}>Dark Mode</h4>
+                          }
+                          <h4 id='githubLink'><a href='https://github.com/victortwu' target='none'>GitHub</a></h4>
+                          <h4 id='linkedInLink'><a href='https://www.linkedin.com/in/victor-twu-6292631b1/' target='none'>LinkedIn</a></h4>
+                          <h4 id='resumeLink'><a href='https://docs.google.com/document/d/17n-inD_l5K_zb6C8aknWffRcIbdIMsC7_K1dTo6vpYM/edit?usp=sharing' target='none'>Resume</a></h4>
+                      </div>
+                  </nav>
 
-                <h1>Victor <span id='twu'>Twu</span></h1>
-                <table className='secondHeading'>
-                  <tbody>
-                    <tr>
-                      <td>
-                            <h3><span id='software'>software</span>Developer<span> () => </span></h3>
-                      </td>
-                      <td id='textLoopCell'>
-                            <h3><span id='skills'>
-                              <TextLoop interval={1000} children={['JavaScript', 'Python', 'CSS3', 'React.js', 'Express.js', 'Node.js', 'MongoDB', 'SQL', 'HTML5']} />
-                            </span></h3>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-          </nav>
+                  <div className='tittleLogo'>
+                        <h1>Victor <span id='twu'>Twu</span></h1>
+                        <table className='secondHeading'>
+                          <tbody>
+                            <tr>
+                              <td>
+                                    <h3><span id='software'>software</span>Developer<span> () => </span></h3>
+                              </td>
+                              <td id='textLoopCell'>
+                                    <h3><span id='skills'>
+                                      <TextLoop interval={1000} children={['JavaScript', 'Python', 'CSS3', 'React.js', 'Express.js', 'Node.js', 'MongoDB', 'SQL', 'HTML5']} />
+                                    </span></h3>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                  </div>
 
             <div className='projectHeading'>
                   featured projects
             </div>
 
               <div className='projectsDiv'>
-
+                <div className='proj1Div'>
                   <ProjectOne/>
+                </div>
+                <div className='proj2Div'>
                   <ProjectTwo/>
+                </div>
+                <div className='proj3Div'>
                   <ProjectThree/>
+                </div>
 
               </div>
 
+
               <div className='brandStatementDiv'>
+                <div className='aboutTitle'/>
                   <h4 id='about'>ABOUT</h4>
+                </div>
+                <div className='aboutContent'>
                   <p>Full stack software engineer and restaurateur with frontend and backend experience.  Will steward data from third parties such as UberEats, Doordash to the restaurant owner's bookeeping system.</p>
                   <p>I built my restaurant during the 2008 financial crisis and am surviving the restaurant through a global pandemic - a testament to my grit, determination, adaptability and even-tempered leadership. I bring strong people skills, business savvy and a forward thinking attitude. </p>
+                </div>
               </div>
 
 
@@ -89,7 +101,8 @@ render() {
             <footer>
             <h1> footer</h1>
             </footer>
-          </div>
+
+
         </main>
       )
     }
