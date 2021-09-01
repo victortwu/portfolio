81888c4936278c2adf99fae4ee58300818fc505a
victortwu <victor607@gmail.com>
2021-08-31 17:10:37 -0700

    added prospective property to cards

--- src/components/ProjectZero.js
+++ src/components/ProjectZero.js
@@ -37,38 +37,3 @@ const ProjectZero =()=> {
 }
 
 export default ProjectZero
-// <div className='flipCardContainer'>
-//
-//   <div className={toggleClass}>
-//
-//     <div className='frontPanel' style={style}>
-//       <h3 id='projTitle'>{projectData.title}</h3>
-//     </div>
-//
-//     <div className='backPanel'>
-//       <p>{projectData.description}</p>
-//       <div className='techListContainer'>
-//         <h4>Technologies Used:</h4>
-//         <div className='techList'>
-//         {
-//           projectData.tech.map((tech, i) => {
-//             return(
-//               <div key={i + tech[i]}>{tech}</div>
-//             )
-//           })
-//         }
-//         </div>
-//       </div>
-//     </div>
-//
-//   </div>
-//
-//   <div className='cardButtonContainer'>
-//     <a href={projectData.link} target='none'><li id='projectLink'>View Project</li></a>
-//     {
-//       flipCard ? <li id='flipButton' onClick={()=> flipBack()}>Flip Back</li>
-//       : <li id='flipButton' onClick={()=> flipOver()}>Description</li>
-//     }
-//   </div>
-//
-// </div>
