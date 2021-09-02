a247db73d70fa6895297372fa13326016fcb9a0e
victortwu <victor607@gmail.com>
2021-09-01 22:38:41 -0700

    linked the cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,41 +2,44 @@
 const Card = (props) => {
 
   return (
-    <div className='flipCardContainer'>
 
-      <div className={props.toggleClass}>
 
-        <div className='frontPanel' style={props.style}>
-          <h3 id='projTitle'>{props.projectData.title}</h3>
-        </div>
+          <div className='flipCardContainer'>
 
-        <div className='backPanel'>
-          <p>{props.projectData.description}</p>
-          <div className='techListContainer'>
-            <h4>Technologies Used:</h4>
-            <div className='techList'>
-            {
-              props.projectData.tech.map((tech, i) => {
-                return(
-                  <div key={i + tech[i]}>{tech}</div>
-                )
-              })
-            }
+            <div className={props.toggleClass}>
+            <a href={props.projectData.link} target='none'>
+              <div className='frontPanel' style={props.style}>
+                <h3 id='projTitle'>{props.projectData.title}</h3>
+              </div>
+
+              <div className='backPanel'>
+                <p>{props.projectData.description}</p>
+                <div className='techListContainer'>
+                  <h4>Technologies Used:</h4>
+                  <div className='techList'>
+                  {
+                    props.projectData.tech.map((tech, i) => {
+                      return(
+                        <div key={i + tech[i]}>{tech}</div>
+                      )
+                    })
+                  }
+                  </div>
+                </div>
+              </div>
+              </a>
             </div>
+
+            <div className='cardButtonContainer'>
+              <a href={props.projectData.link} target='none'><button id='projectLink'>View Project</button></a>
+              {
+                props.flipCard ? <button id='flipButton' onClick={()=> props.flipBack()}>Flip Back</button>
+                : <button id='flipButton' onClick={()=> props.flipOver()}>Description</button>
+              }
+            </div>
+
           </div>
-        </div>
 
-      </div>
-
-      <div className='cardButtonContainer'>
-        <a href={props.projectData.link} target='none'><button id='projectLink'>View Project</button></a>
-        {
-          props.flipCard ? <button id='flipButton' onClick={()=> props.flipBack()}>Flip Back</button>
-          : <button id='flipButton' onClick={()=> props.flipOver()}>Description</button>
-        }
-      </div>
-
-    </div>
   )
 }
 
